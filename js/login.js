204991623a3292a1c647496fb3462d45cb712356
AlejandroPerezAnuncibay <alejandro.perez@ikasle.egibide.org>
2020-10-05 20:05:53 +0200

    Login finalizado

--- js/login.js
+++ js/login.js
@@ -1,29 +1,29 @@
 var admin = {nm:'admin', pw:'admin'}
-
+// Creación de usuario admin para que exista
 var arrayU = [];
 
 arrayU.push(admin);
 
-localStorage.setItem('arrayU',JSON.stringify(arrayU))
+localStorage.setItem('arrayU',JSON.stringify(arrayU));
 function registro() {
+    // Creación de un objeto Usuario con los elementos del HTML
     let us = {nm:document.getElementById('nm').value, pw:document.getElementById('pw').value};
 
     let arrayUsuario = JSON.parse(localStorage.getItem('arrayU'));
 
     arrayUsuario.push(us);
+
     localStorage.setItem('arrayU',JSON.stringify(arrayUsuario));
-    let arrayP = JSON.parse(localStorage.getItem('arrayU'));
-    for (x =0 ; x<arrayP.length; x++)
-        alert(arrayP[x].nm)
+
 
 
 }
 
 function comprobar() {
-    var arrayU = JSON.parse(localStorage.getItem('arrayU'));
+    let arrayU = JSON.parse(localStorage.getItem('arrayU'));
 
-    var nmUsu = document.getElementById('userName').value;
-    var pwUsu = document.getElementById('userPw').value;
+    var nmUsu = document.getElementById('nm').value;
+    var pwUsu = document.getElementById('pw').value;
     let u = arrayU.find (u => u.nm == nmUsu && u.pw == pwUsu)
     if(u == undefined) {
         alert('Usuario y/o contraseña incorrecto');
